695dd0a7772353d4a5ec6931a6ab58f180d45a2f
Abo7mood <74455601+Abo7mood@users.noreply.github.com>
2025-02-06 17:18:35 +0300

    project

---
+++ Assets/CheckDissolver.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckDissolver : MonoBehaviour
+{
+
+    [SerializeField] GameObject[] TubesGroup;
+
+    private void Awake()
+    {
+        int rand = Random.Range(0, 6);
+        TubesGroup[rand].SetActive(true);
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player"))
+            return;
+        else
+        {
+            Destroy(this.gameObject, 10f);
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(this.gameObject, 10f);
+        }
+          
+    }
+}
---
+++ Assets/Scripts/FlappyBird.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+public class FlappyBird : MonoBehaviour
+{
+    #region Constructer
+
+    [SerializeField] GameObject PrefabBackGround;
+    [SerializeField] Transform MainPosition;
+    [SerializeField] Transform[] Group;
+    [SerializeField] Vector3 bro;
+    [SerializeField] GameObject[] A;
+    [SerializeField] GameObject[] Transforms;
+    [SerializeField] GameObject DiePanel;
+    [SerializeField] TextMeshProUGUI TXT;
+
+   
+    GameObject BackGround;
+    Rigidbody2D rb;
+    SpriteRenderer sprite;
+   public Camera cam;
+    #endregion
+    #region floats&int
+    [Header("Movement")]
+    [SerializeField] float speed;
+    [SerializeField] float jumpvalue;
+    [SerializeField] float TimeRepeating;
+    [SerializeField] float Directionx;
+    [SerializeField] float Directiony;
+  
+
+    private int CoinsAmount=0;
+    #endregion
+    #region Boolean
+ 
+    
+    #endregion
+    private void Awake()
+    { 
+           cam = Camera.main;
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+    private void Start()
+    {
+ 
+        A = GameObject.FindGameObjectsWithTag("A");
+        Transforms = GameObject.FindGameObjectsWithTag("Transforms");
+        InvokeRepeating("InstintiateObject", 0.01f, TimeRepeating);
+        Time.timeScale = 0;
+    }
+    private void Update()
+    {
+        A= GameObject.FindGameObjectsWithTag("A");
+        Transforms = GameObject.FindGameObjectsWithTag("Transforms");
+
+        TXT.text = CoinsAmount.ToString();
+
+#if UNITY_EDITOR
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0))
+            jump();
+#endif
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        
+            jump();
+        
+
+
+
+
+        transform.Translate(bro * Time.deltaTime);
+        cam.transform.Translate(bro * Time.deltaTime);
+        Group[0].Translate(bro * Time.deltaTime);
+
+
+
+
+    }
+    private void FixedUpdate()
+    {
+        float maxVelocity = 10;      
+            rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxVelocity);      
+    }
+    public void InstintiateObject()=> BackGround = Instantiate(PrefabBackGround, A[A.Length-1].transform.position, Quaternion.identity, null);
+    
+        
+   
+    void jump()=> rb.velocity=new Vector2(rb.velocity.x,jumpvalue);
+    
+      
+        
+    
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Die"))
+        {
+           
+                Die();
+          
+        }
+        else if (collision.CompareTag("Coins"))
+        {
+            Destroy(collision.gameObject, 0);
+            CoinsAmount++;
+            Time.timeScale += 0.05f;
+        }
+
+
+
+
+    }
+    public void StartGame()=> Time.timeScale = 1;
+    private void Die()
+    {
+        Time.timeScale = 0;
+        DiePanel.SetActive(true);
+    }
+    public void StartTheGame() => SceneManager.LoadScene(0);
+  
+}
